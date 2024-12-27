baa353607b86354ea9786047dcdcde0211c67c07
assassinLx <874032390@qq.com>
2024-12-27 17:51:52 +0800

    Drawing Debug Spheres And Drawing Debug Lines

--- Source/Map05/Map05.h
+++ Source/Map05/Map05.h
@@ -3,4 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "DrawDebugHelpers.h"
 
+#define DRAW_SPHERE(Location) if(GetWorld()) DrawDebugSphere(GetWorld(),Location,25.f,12,FColor::Red,true);
+
+#define DRAW_LINE(SP,EP,W) if(GetWorld()) DrawDebugLine(GetWorld(),SP,EP,FColor::Red,true,-1.f,0,W);
