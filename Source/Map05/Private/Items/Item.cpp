58dd50cd0e32384e36d3bc3738bc73e1125175ee
assassinLx <874032390@qq.com>
2024-12-31 17:19:14 +0800

    Components in C++

--- Source/Map05/Private/Items/Item.cpp
+++ Source/Map05/Private/Items/Item.cpp
@@ -9,6 +9,10 @@ AItem::AItem()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	//如果不被指向，由于它不会有任何指向它的指针，GC会回收
+	RootComponent = ItemMesh;
 
 }
 
@@ -54,16 +58,18 @@ void AItem::Tick(float DeltaTime)
 	//AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));
 	//AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f));
 	
-	FVector localPostion = GetActorLocation();
-	FVector endLocalPostion = localPostion + GetActorForwardVector() * 100.f;
-	DRAW_SPHERE_SingleFrame(localPostion);
-	DRAW_VECTOR_SingleFrame(localPostion, endLocalPostion);
+	//FVector localPostion = GetActorLocation();
+	//FVector endLocalPostion = localPostion + GetActorForwardVector() * 100.f;
+	//DRAW_SPHERE_SingleFrame(localPostion);
+	//DRAW_VECTOR_SingleFrame(localPostion, endLocalPostion);
 
 	RunningTime += DeltaTime;
 	//float DeltaZ = Amplitude * FMath::Sin(Value * TimeConstant);
 	//AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
 
-	FVector AvgVector = Avg<FVector>(GetActorLocation(), FVector::ZeroVector);
-	DRAW_POINT_SingleFrame(AvgVector);
+	//FVector AvgVector = Avg<FVector>(GetActorLocation(), FVector::ZeroVector);
+	//DRAW_POINT_SingleFrame(AvgVector);
+
+
 }
 
